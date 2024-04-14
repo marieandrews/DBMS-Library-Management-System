8053fd5a0292203f817622991f4ded3123748ae1
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-04-14 10:25:06 -0400

    create procedure to view current user balance

---
+++ StoredProcedures/ViewUserBalance/spViewBalance.sql
@@ -0,0 +1,23 @@
+Drop procedure spViewBalance;
+
+CREATE PROCEDURE spViewBalance
+	@Username NVARCHAR(20)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM USERS WHERE Username = @Username)
+	BEGIN
+		SELECT balance as Current_Balance, 1 AS ISSUCCESS from Users where Username = @Username;
+	END
+	ELSE
+	BEGIN
+		SELECT 'Invalid username!' AS MESSAGE, 0 AS ISSUCCESS;
+	END
+END;
+go
+
+EXEC spViewBalance
+    @Username = 'testuser'
+
+select * from users;
